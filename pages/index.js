c7117642ca2dded50642febd3bcb62125a93ce75
JomanaAhmed01 <66492958+JomanaAhmed01@users.noreply.github.com>
2021-07-05 17:25:54 +0200

    Creating 1st Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useState } from "react"
+import Navbar1 from "../components/Navbar/Navbar1"
 
 export default function Home() {
+  const [showSendMessageModal, setShowSendMessageModal] = useState(false)
+  const [showTradingModal, setShowTradingModal] = useState(false)
+
+  function handleClickModal() {
+    setShowSendMessageModal(true)
+  }
+
+  function handleCloseModal() {
+    setShowSendMessageModal(false)
+  }
+
+  function handleClickTrading() {
+    setShowTradingModal(true)
+  }
+
+  function handleCloseTrading() {
+    setShowTradingModal(false)
+  }
+
   return (
-    <></>
+    <>
+      <Navbar1
+        handleClickModal={handleClickModal}
+        showSendMessageModal={showSendMessageModal}
+      />
+    </>
   )
 }
